95607be4d07ab3efd0e50fa49891c6b48c4b4617
Lasantha Basnayake <lasantha57@gmail.com>
2020-01-28 03:20:17 +0800

    style: tweak input, navigation look

--- src/app/shared/controls/Input.jsx
+++ src/app/shared/controls/Input.jsx
@@ -3,7 +3,7 @@ import './Input.css';
 
 const Input = ({ className, ...restProps }) => {
     return (
-        <input className={`form__input ${className || ''}`} {...restProps} />
+        <input className={`form__input ${className || ''}`} {...restProps} style={{ textTransform: "uppercase" }} />
     )
 }
 
--- src/app/shared/navigation/Navigation.jsx
+++ src/app/shared/navigation/Navigation.jsx
@@ -6,8 +6,8 @@ import './Navigation.css';
 const Navigation = () => {
     return (
         <div className="navigation">
-            <NavLink className="nav" to="routes" activeClassName="nav--active">Eko Routes</NavLink>
-            <NavLink className="nav" to="delivery-cost" activeClassName="nav--active">Delivery Cost</NavLink>
+            <NavLink className="nav" to="routes" activeClassName="nav--active">Eko Routes</NavLink> |
+            <NavLink className="nav" to="delivery-cost" activeClassName="nav--active">Delivery Cost</NavLink> |
             <NavLink className="nav" to="delivery-routes" activeClassName="nav--active">Delivery Routes</NavLink>
         </div>
     )
