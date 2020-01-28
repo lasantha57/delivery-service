718ab94643b7d8297b0d925314ae6c12f7e891a2
Lasantha Basnayake <lasantha57@gmail.com>
2020-01-28 13:07:02 +0800

    feat: add delivery cost page functions

--- src/app/delivery-cost/DeliveryCost.jsx
+++ src/app/delivery-cost/DeliveryCost.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getDeliveryCost } from '../../helpers/route-helper';
 
 import Button from '../shared/controls/Button';
 import Input from '../shared/controls/Input';
 
 const DeliveryCost = () => {
+
+    const [route, setRoute] = useState('');
+    const [cost, setCost] = useState(0);
+
+    const findCostHandler = () => {
+        setCost(getDeliveryCost());
+    }
+
     return (
         <div className="tab">
             <h4>Find your Cost for Delivery Route</h4>
             <div className="container">
                 <form className="form">
                     <div className="form__group">
-                        <Input id="route" type="text" name="route" placeholder="Route"></Input>
+                        <Input id="route" type="text" name="route" placeholder="Route" value={route} onChange={(e) => setRoute(e.target.value.toUpperCase())}></Input>
                     </div>
                     <div>
-                        <Button id="find" type="button">Find Cost</Button>
+                        <Button id="find" type="button" onClick={findCostHandler}>Find Cost</Button>
                     </div>
                 </form>
             </div>
             <div className="container">
-
+                <div>Your cost is {cost}</div>
             </div>
         </div>
     )
